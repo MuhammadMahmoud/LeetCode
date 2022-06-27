cd613b74038293869c12d899a70421accadaeee9
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-06-27 22:18:10 +0200

    Time: 10 ms (87.98%), Space: 10.6 MB (94.74%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        // (r * c)
+        int rr = mat.size(), cc = mat[0].size();
+        if(rr * cc != r * c) return mat;
         
-        if(mat.size()*mat[0].size() == r*c){
-            vector<int> flat;
-            for(int i=0;i<mat.size();i++){
-                for(int j=0;j<mat[i].size();j++){
-                    flat.push_back(mat[i][j]);
-                }
-                mat[i].clear();
-            }
-            mat.clear();
-            cout<<mat.size()<<endl;
-            for(int i=0,count=0;i<r;i++){
-                mat.push_back(vector<int>());
-                for(int j=0;j<c;j++){
-                    mat[i].push_back(flat[count]);
-                    count++;
-                }
-            }
-            return mat;
-        }
-        else{
-            return mat;
-        }
+        vector<vector<int>> res(r, vector<int>(c));
+        for(int i = 0; i < r * c; i++) res[i / c][i % c] = mat[i / cc][i % cc];
+        
+        return res;
     }
 };
