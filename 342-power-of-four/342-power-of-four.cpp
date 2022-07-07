d10aa7b7c09b02db5fbb56593cfbc613c1526ee4
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-07 22:17:18 +0200

    Time: 4 ms (39.50%), Space: 6 MB (24.31%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        // bool isPow=false;
+        if(n == 1){
+            return true;
+        }
+        else if(n<=0){
+            return false;
+        }
+        int unsigned num=4;
+        for(int i=0;i<sizeof(int)*8;i+=2){
+            if(n == num){
+                return true;
+            }
+            num<<=2;
+        }
+        
+        return false;
+    }
+};
