4b215d7958bb483f0dc4350dc84d706049f63a20
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-11 05:45:01 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (73.87%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num == 0){
+            return 0;
+        }
+        else if(num/10 ==0){
+            return num;
+        }
+        int sum=0,count=0;
+        int temp;
+        
+        while(num/10 != 0){
+            temp=num;
+            sum=0;
+            while(temp){
+                sum+=(temp%10);
+                temp/=10;
+            }
+            num=sum;
+        }
+
+        return sum;
+    }
+};
