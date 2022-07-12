ec7e3cfd75701ec837a04431f88bf54c53fb76b1
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-12 05:13:07 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (76.06%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res=0;
+        // cout<<INT_MAX<<endl;
+        while(x){
+            int pop = x % 10;
+            x/=10;
+            if (res > INT_MAX/10 || (res == INT_MAX / 10 && pop > 7)) return 0;
+            if (res < INT_MIN/10 || (res == INT_MIN / 10 && pop < -8)) return 0;
+            res=(res*10)+pop;
+        }
+        return res;
+    }
+};
