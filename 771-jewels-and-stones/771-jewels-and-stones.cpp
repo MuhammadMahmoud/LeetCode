f182e37bebce9e3b61ffcb09d9d2a898df48710e
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-22 03:37:53 +0200

    Time: 5 ms (29.30%), Space: 6.5 MB (26.99%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> s;
+        int count=0;
+        for(int i=0;i<jewels.size();i++){
+            s.insert(jewels[i]);
+        }
+        for(int i=0;i<stones.size();i++){
+            if(s.count(stones[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+};
