5474ae8739b9793469dde8daace519338068b6ea
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-23 16:32:04 +0200

    Time: 46 ms (64.23%), Space: 24.2 MB (5.01%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,18 +1,26 @@
 class KthLargest {
-private:
-    multiset<int> items;
-    int kth;
 public:
+    // O(NLOGN) time, O(N) space
+    multiset<int> s1, s2; // O(N) space
+    int k;
     KthLargest(int k, vector<int>& nums) {
-        for(int num:nums) items.insert(num);
-        this->kth=k;
+        this->k = k;
+        for(int num: nums) s1.insert(num);  // O(NLOGN) time
+        while(s2.size() < k && s1.size()) s2.insert(*(s1.rbegin())), s1.erase(--s1.end());  // O(KLOGN)
     }
     
-    int add(int val) {
-        items.insert(val);
-        int count=0;
-        for(int num:items) if(count++ == items.size()-kth) return num;
-        return 0;
+    void balance() {  // O(LOGN)
+        if(s2.size() > k) s1.insert(*(s2.begin())), s2.erase(s2.begin());
+    }
+    
+    int add(int val) {  // O(LOGN)
+        if(s2.size() < k || val > *(s2.begin())) {
+            s2.insert(val), balance();
+        } else {
+            s1.insert(val);
+        }
+        // (val > *(s2.begin())) ? s2.insert(val), balance() : s1.insert(val);
+        return *(s2.begin());
     }
 };
 
