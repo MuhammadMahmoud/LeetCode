28e9be3cc2dce5ac173a5ca37b67f60c9e8e8561
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-30 15:32:19 +0200

    Time: 191 ms (17.91%), Space: 59.7 MB (7.16%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -6,9 +6,9 @@ public:
         int m;
         // 1 8 5 4 3
         while(r>l){
-            m=ceil((r+l)/2.0);
-            if (arr[m] < arr[m-1]) r=m-1;
-            else l=m;
+            m=(r+l)/2;
+            if (arr[m] > arr[m+1]) r=m;
+            else l=m+1;
             
         }
         return r;
