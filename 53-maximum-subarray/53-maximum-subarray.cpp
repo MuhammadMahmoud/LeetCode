42330a7ba250d5e46d8b12323223ea6170709f12
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-30 23:04:18 +0200

    Time: 117 ms (94.30%), Space: 67.7 MB (90.72%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=0,maxsum=-10000,count;
+        int sum=0,maxsum=-10000,reset;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            count=nums[i];
-            if(count>sum) sum=count;
+            reset=nums[i];
+            if(reset>sum) sum=reset;
             if(sum>maxsum) maxsum=sum;
                 
         }
