82f747edda6bbda3aaa2500c07ffd863481779de
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-07-30 15:08:11 +0200

    Time: 166 ms (25.54%), Space: 59.7 MB (7.16%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -4,27 +4,14 @@ public:
         if(arr.size()<3) return -1;
         int r=arr.size()-1,l=0;
         int m;
+        // 1 8 5 4 3
         while(r>=l){
             m=(r+l)/2;
-            if(m>0 && m< arr.size()-1){
-                if(arr[m]> arr[m-1] && arr[m] > arr[m+1]) return m;
-                else if (arr[m] > arr[m-1] && arr[m] < arr[m+1]) l=m+1;
-                else if (arr[m] < arr[m-1] && arr[m] > arr[m+1]) r=m-1;
-            }
-            else if( m == 0 && arr[m+1]>arr[m] && arr[m+2]<arr[m+1]){
-                return m+1;
-            }
-            else if((m == 0 && arr[m+1]>arr[m] && arr[m+2]>arr[m+1]) ||
-                   (m == 0 && arr[m+1]<arr[m] && arr[m+2]<arr[m+1]) ){
-                return -1;
-            }
-            else if(m == arr.size()-1 && arr[m-1]>arr[m] && arr[m-2]<arr[m-1]){
-                return m-1;
-            }
-            else if((m == arr.size()-1 && arr[m-1]>arr[m] && arr[m-2]>arr[m-1]) ||
-                   (m == arr.size()-1 && arr[m-1]<arr[m] && arr[m-2]<arr[m-1])){
-                return -1;
-            }
+            
+            if(arr[m]> arr[m-1] && arr[m] > arr[m+1]) return m;
+            else if (arr[m] > arr[m-1] && arr[m] < arr[m+1]) l=m+1;
+            else if (arr[m] < arr[m-1] && arr[m] > arr[m+1]) r=m;
+            
         }
         return -1;
     }
