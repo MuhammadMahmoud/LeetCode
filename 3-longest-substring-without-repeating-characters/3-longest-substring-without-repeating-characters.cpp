02f5335f432648f31a5d551701b8f7297a18bec9
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-08-06 23:06:47 +0200

    Time: 28 ms (50.15%), Space: 8.2 MB (81.99%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,19 +3,13 @@ public:
     int lengthOfLongestSubstring(string s) {
         int max=0;
         int flag=0;
-        //"abcbcbb"
         unordered_map<char,int> m;
-            for(int i=0,j=0;j<s.size();j++){
-                m[s[j]]++;
-                if(m[s[j]] >= 2){
-                    while(m[s[j]] >= 2){
-                        m[s[i]]--;
-                        i++;
-                        
-                    }
-                }
-                if(j-i+1 > max) max=j-i+1;
-            }
+        for(int i=0,j=0;j<s.size();j++){
+            m[s[j]]++;
+            if(m[s[j]] >= 2)
+                while(m[s[j]] >= 2) m[s[i]]--,i++;
+            if(j-i+1 > max) max=j-i+1;
+        }
         return max;
     }
 };
