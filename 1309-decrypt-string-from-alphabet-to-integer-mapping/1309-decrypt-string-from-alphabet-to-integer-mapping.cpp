ee5e18960d3af180e69f9dca2abf390ec431a4aa
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-08-09 14:06:46 +0200

    Time: 3 ms (50.71%), Space: 6.3 MB (43.14%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -3,9 +3,8 @@ public:
     string freqAlphabets(string s) {
         string freq="";
         for(int i=0;i<s.size();i++){
-            if(i + 2 < s.size() && ( s[i+2] != '#')){
+            if(i + 2 < s.size() && ( s[i+2] != '#'))
                 freq+=s[i]+48;
-            }
             else if(i + 2 < s.size() && ( s[i+2] == '#')){
                 if(s[i] == '1')
                     freq+='i'+(s[i+1]-'0'+1);
@@ -13,9 +12,8 @@ public:
                     freq+='s'+(s[i+1]-'0'+1);
                 i+=2;
             }
-            else if(i < s.size()){
+            else if(i < s.size())
                 freq+=s[i]+48;
-            }
         }
         return freq;
     }
