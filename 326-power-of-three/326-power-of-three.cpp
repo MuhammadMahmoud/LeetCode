d1c1c5b758ede235f3a3641fd67908096038f3a3
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-08-28 18:13:05 +0200

    Time: 31 ms (59.51%), Space: 5.7 MB (88.84%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n == 1){
-            return true;
-        }
-        if(n==0){
+        if(n == 0)
             return false;
-        }
-        
-        while(n%3 == 0){
-            if(n==3){
-                return true;
-            }
-            n/=3;
-        }
-        return false;
+        else if(n == 1 || n==3)
+            return true;
+        else if(n%3 == 0)
+            return isPowerOfThree(n/3);
+        else return false;
     }
 };
