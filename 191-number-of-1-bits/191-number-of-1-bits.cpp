57005efd72af0be49449464de175b0a79657acf0
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-09-05 19:14:03 +0200

    Time: 0 ms (100.00%), Space: 6 MB (47.49%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count=0;
-        for(int i=0;i<(sizeof(uint32_t)*8);i++){
-            if(n & 1 == 1){
-                count++;
-            }
-            n>>=1;
-        }
-        return count;
+        if(n==0) return 0;
+        else
+            return (n%2) + hammingWeight(n/2);
     }
 };
