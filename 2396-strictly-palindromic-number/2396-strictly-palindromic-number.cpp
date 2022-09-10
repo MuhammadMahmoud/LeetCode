281fa4a746141030c0467970d5bbc4a87236ddd3
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2022-09-10 18:07:07 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (77.70%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+//     bool palind(int n){
+//         int size=sizeof(int)*8;
+//         while(--size){
+//             if((n>>size)&1) break;
+//             else size--;
+//         }
+//         size++;
+//         for(int i=0;i<size/2;i++){
+//             if( (n>>i)&1 != (n>>(size-i-1)) ) return false;
+            
+//         }
+//         return true;
+//     }
+    bool isStrictlyPalindromic(int n) {
+            
+        // return palind(9);
+//         for(int i=2;i<=n-2;i++){
+            
+//         }
+        return false;
+    }
+};
